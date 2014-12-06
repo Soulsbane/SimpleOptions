31f9a9fef1e9ba4d370df26f4cd964fc72105660
Paul Crane <paul@acheronsoft.com>
2014-12-06 13:42:16 -0600

    Initial commit

---
+++ SimpleOptions.lua
@@ -0,0 +1,152 @@
+--NOTE: This is very much a work in progress. Some features may be added/changed etc at any time.
+local AddonName, Addon = ...
+
+local Options = {}
+local OptionsFrame
+
+local function DispatchMethod(func, ...)
+	if type(func) == "string" and Addon[func] then
+		Addon[func](Addon, ...)
+	elseif type(func) == "function" then
+		func(...)
+	end
+end
+
+local function CreateWidgetFrame(frameType, name, template)
+	return CreateFrame(frameType, OptionsFrame:GetName() .. name, OptionsFrame, template)
+end
+
+function Options:AttachRight(frame, attachedTo, offsetX, offsetY)
+	local offsetX = offsetX or -30
+	local offsetY = offsetY or 0
+
+	frame:SetPoint("RIGHT", attachedTo, "RIGHT", offsetX, offsetY)
+end
+
+function Options:AttachLeft(frame, attachedTo, offset)
+	local offsetX = offsetX or 30
+	local offsetY = offsetY or 0
+
+	frame:SetPoint("LEFT", attachedTo, "LEFT", offsetX, offsetY)
+end
+
+function Options:AttachAbove(frame, attachedTo, offsetX, offsetY)
+	local offsetX = offsetX or 0
+	local offsetY = offsetY or 60
+
+	frame:SetPoint("TOP", attachedTo, "TOP", offsetX, offsetY)
+end
+
+function Options:AttachBelow(frame, attachedTo, offsetX, offsetY)
+	local offsetX = offsetX or 0
+	local offsetY = offsetY or -60
+
+	frame:SetPoint("BOTTOM", attachedTo, "BOTTOM", offsetX, offsetY)
+end
+
+function Options:CreatePanel(title, icon)
+	local name = AddonName .. "OptionsFrame"
+	local version = GetAddOnMetadata(AddonName, "Version") or ""
+	local title = title or AddonName
+
+	OptionsFrame = CreateFrame("Frame", name, InterfaceOptionsFramePanelContainer)
+	OptionsFrame.name = AddonName
+
+	local text = OptionsFrame:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+	text:SetPoint("TOPLEFT", 16, -16)
+
+	if icon then
+		text:SetFormattedText("|T%s:%d|t %s %s", icon, 16, title, version)
+	else
+		text:SetText(title .. version)
+	end
+
+	InterfaceOptions_AddCategory(OptionsFrame)
+	return OptionsFrame
+end
+
+function Options:AddButton(name, width, height)
+	local button = CreateFrame("Button", OptionsFrame:GetName() .. name, OptionsFrame, "UIPanelButtonTemplate")
+
+	button:SetText(name)
+	button:SetWidth(button:GetTextWidth() + 20)
+	button:SetScript("OnClick", function(self)
+		DispatchMethod("On" .. name .. "Clicked")
+	end)
+
+	return button
+end
+
+function Options:AddCheckButton(name, label)
+	local button = CreateWidgetFrame("CheckButton", name, "InterfaceOptionsCheckButtonTemplate")
+	local label = label or name
+
+	_G[button:GetName() .. "Text"]:SetText(label)
+	button:SetScript("OnClick", function(self)
+		DispatchMethod("On" .. name .. "Clicked", button:GetChecked())
+	end)
+
+	return button
+end
+
+function Options:AddEditBox(name, width, height)
+	local width = width or 30
+	local height = height or 20
+	local editBox = CreateWidgetFrame("EditBox", name, "InputBoxTemplate")
+
+	editBox:SetWidth(width)
+	editBox:SetHeight(height)
+	editBox:SetAutoFocus(false)
+
+	editBox:SetScript("OnTextChanged", function(self, userInput)
+		DispatchMethod("On" .. name .. "ValueChanged", editBox:GetText())
+	end)
+
+	editBox:SetScript("OnEnterPressed", function(self, userInput)
+		DispatchMethod("On" .. name .. "EnterPressed", editBox:GetText())
+	end)
+
+	return editBox
+end
+
+function Options:AddSlider(name, min, max, startValue, step)
+	local frameName = OptionsFrame:GetName() .. name
+	local slider = CreateWidgetFrame("Slider", name, "OptionsSliderTemplate")
+
+	slider:SetMinMaxValues(min, max)
+	slider:SetValue(startValue)
+	slider:SetValueStep(step)
+	_G[frameName.."Low"]:SetText(tostring(min)) --FIXME: We should really add parameters for these three.
+	_G[frameName.."High"]:SetText(tostring(max))
+	_G[frameName.."Text"]:SetText(name)
+
+	slider:SetScript("OnValueChanged", function(self, value)
+		DispatchMethod("On" .. name .. "ValueChanged", value)
+	end)
+
+	return slider
+end
+
+function Options:AddDropDownMenu(name, menuList)
+	local frameName = OptionsFrame:GetName() .. name
+	local dropDownMenuFrame = CreateFrame("Button", frameName, OptionsFrame, "UIDropDownMenuTemplate")
+
+ 	UIDropDownMenu_Initialize(dropDownMenuFrame, function(self)
+		local info = UIDropDownMenu_CreateInfo()
+
+		for k,v in pairs(menuList) do
+			info = UIDropDownMenu_CreateInfo()
+			info.text = v
+			info.value = v
+			info.func = function(self)
+				UIDropDownMenu_SetSelectedID(dropDownMenuFrame, self:GetID())
+				DispatchMethod("On" .. name .. "ValueChanged", self:GetText(), self:GetID())
+			end
+			UIDropDownMenu_AddButton(info, level)
+		end
+	end)
+
+	return dropDownMenuFrame
+end
+
+Addon.Options = Options
---
+++ Tests.lua
@@ -0,0 +1,75 @@
+local AddonName, Addon = ...
+local Options = Addon.Options
+local Events = CreateFrame("Frame", nil, UIParent)
+
+local function ADDON_LOADED(frame, event, ...)
+	if ... == AddonName then
+		Options:CreatePanel()
+
+		local testButton1 = Options:AddButton("TestButton1")
+		testButton1:SetPoint("TOPLEFT", 12, -72)
+
+		local testButton2 = Options:AddButton("TestButton2")
+		Options:AttachBelow(testButton2, testButton1)
+
+		local testCheckButton1 = Options:AddCheckButton("TestCheckButton1", "Hello checkbutton")
+		Options:AttachRight(testCheckButton1, testButton2, 70)
+
+		local testSlider1 = Options:AddSlider("TestSlider1", 1, 100, 1, 1)
+		Options:AttachBelow(testSlider1, testCheckButton1)
+
+		local testEditBox1 = Options:AddEditBox("TestEditBox1")
+		Options:AttachAbove(testEditBox1, testCheckButton1)
+
+		local menu = {
+			"Alpha",
+			"Beta",
+			"Gamma",
+			"Delta",
+		 }
+
+		local testDropDownMenu1 = Options:AddDropDownMenu("TestDropDownMenu1", menu)
+		Options:AttachBelow(testDropDownMenu1, testSlider1)
+	end
+end
+
+function Addon:OnTestCheckButton1Clicked(enabled)
+	if enabled then
+		print("OnTestCheckButton1Clicked: is enabled")
+	else
+		print("OnTestCheckButton1Clicked: is disabled")
+	end
+end
+
+function Addon:OnTestButton1Clicked()
+	print("TestButton1 clicked!")
+end
+
+function Addon:OnTestButton2Clicked()
+	print("TestButton2 clicked!")
+end
+
+function Addon:OnTestButton3Clicked()
+	print("TestButton3 clicked!")
+end
+
+function Addon:OnTestSlider1ValueChanged(value)
+	print("TestSlider1 value changed: " .. tostring(floor(value)))
+end
+
+function Addon:OnTestEditBox1ValueChanged(value)
+	if value ~= "" then
+		print("TestEditbox1 value changed: ", value)
+	end
+end
+
+function Addon:OnTestEditBox1EnterPressed(value)
+	print("TestEditbox1 Enter pressed: ", value)
+end
+
+function Addon:OnTestDropDownMenu1ValueChanged(value, id)
+	print("TestDropDownMenu1 value changed: ", value, id)
+end
+
+Events:RegisterEvent("ADDON_LOADED")
+Events:SetScript("OnEvent", ADDON_LOADED)
